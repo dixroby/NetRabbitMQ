e46505baf7c4454ed6ba14d2702e97632c0af0ff
Dixroby <dixroby.arone@itsight.pe>
2022-08-14 10:09:28 -0500

    Agregar archivos de proyecto.

---
+++ NetRabbitMQ.Consumer/Reciver.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+const string queue = "firtsQueue";
+ConnectionFactory factory = new ConnectionFactory
+{
+    HostName = "localhost",
+    UserName = "YOUR_USERNAME",
+    Password = "YOUR_PASSWORD",
+};
+
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare(queue, false, false, false, null);
+
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body.Span;
+        var message = Encoding.UTF8.GetString(body);
+        Console.WriteLine("this is message recived {0}", message);
+    };
+
+    channel.BasicConsume(queue, true, consumer);
+
+    Console.WriteLine("Press [enter] exit aplication");
+}
---
+++ NetRabbitMQ.Producer/Sender.cs
@@ -0,0 +1,25 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+const string queue = "firtsQueue";
+
+ConnectionFactory factory = new ConnectionFactory
+{
+    HostName = "localhost",
+    UserName = "YOUR_USERNAME",
+    Password = "YOUR_PASSWORD",
+};
+
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare(queue, false, false, false, null);
+    var message = "new queue 2022";
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish("", queue, null, body);
+    Console.WriteLine("this is message send {0}", message);
+}
+
+Console.WriteLine("Press enter exit aplication");
+Console.ReadLine();
